d8a042b32693ed9993d5164a595cd7a99d13c573
VarArm <1162397414@qq.com>
2019-07-13 11:52:14 +0800

    two

---
+++ mypmvuedouban/src/pages/more/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="content">
+      <h1>{{ title }}</h1>
+      <div class="imgList">
+        <div v-for="(item, index) in dataList" :key="index" class="item">
+          <img :src="item.images.small" alt />
+          <div class="name">{{ item.title }}</div>
+          <div class="score">
+            <div v-if="item.start != 0">
+              <span v-if="item.start >= 1" class="start1"></span>
+              <span v-else class="start2"></span>
+              <span v-if="item.start >= 2" class="start1"></span>
+              <span v-else class="start2"></span>
+              <span v-if="item.start >= 3" class="start1"></span>
+              <span v-else class="start2"></span>
+              <span v-if="item.start >= 4" class="start1"></span>
+              <span v-else class="start2"></span>
+              <span v-if="item.start >= 5" class="start1"></span>
+              <span v-else class="start2"></span>
+              <span class="word">{{ item.rating.average }}</span>
+            </div>
+            <div v-else>暂无评分</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import wxrequest from "../../utils/js/wxrequest.js";
+export default {
+  data() {
+    return {
+      type: "",
+      tltle: "",
+      url: "https://api.douban.com/v2/movie/",
+      // apikey
+      apikey: "apikey=0df993c66c0c636e29ecbb5344252a4a",
+      dataList: [],
+      // 开始位置
+      start: 0,
+      // 获取条数
+      count: 9
+    };
+  },
+  methods: {
+    async getDataList() {
+      var res = await wxrequest({
+        url: `${this.url}${this.type}?${this.apikey}&start=${
+          this.start
+        }$count=${this.count}`,
+        header: {
+          "content-type": "json"
+        }
+      });
+      let { statusCode, data } = res;
+      if (statusCode === 200) {
+        data.subjects.forEach(item => {
+          item.start = Math.round(item.rating.average / 2);
+          item.rating.average = item.rating.average.toFixed(1);
+        });
+        this.dataList = [...this.dataList, ...this.subjects];
+      }
+    }
+  },
+  onLoad(options) {
+    this.type = options.type;
+    switch (this.type) {
+      case "in_theaters":
+        this.title = "影院热映";
+        break;
+      case "top250":
+        this.title = "top250";
+        break;
+      case "coming_soon":
+        this.title = "即将上映";
+        break;
+    }
+    this.getDataList();
+  }
+};
+</script>
+
+<style scoped lang="less">
+.content {
+  padding: 44rpx;
+  h1 {
+    font-size: 24px;
+  }
+  .imgList {
+    display: flex;
+    flex-wrap: wrap;
+    .item {
+      width: 33.333%;
+      margin-top: 60rpx;
+      text-align: center;
+      img {
+        width: 186rpx;
+        height: 260rpx;
+      }
+      .name {
+        font-size: 16px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+      .score {
+        font-size: 16px;
+        span {
+          display: inline-block;
+          width: 20rpx;
+          height: 20rpx;
+          background-size: 100%;
+        }
+        .start1 {
+          background-image: url(../../utils/imgs/start1.png);
+        }
+        .start2 {
+          background-image: url(../../utils/imgs/start2.png);
+        }
+      }
+    }
+  }
+}
+</style>
--- mypmvuedouban/src/pages/movieList/index.vue
+++ mypmvuedouban/src/pages/movieList/index.vue
@@ -1,32 +1,111 @@
 <template>
   <div class="content">
-    <div class="mybox">
-      <scroll-view scroll-x class="myscroll">
-        <div class="item" v-for="(item, index) in movieList" :key="index">
-          <img :src="item.images.small" alt />
-          <div class="moviename">{{ item.title }}</div>
-          <div class="score">
-            <div v-if="item.start != 0">
-              <span v-if="item.start >= 1" class="start1"></span>
-              <span v-else class="start2"></span>
-              <span v-if="item.start >= 2" class="start1"></span>
-              <span v-else class="start2"></span>
-              <span v-if="item.start >= 3" class="start1"></span>
-              <span v-else class="start2"></span>
-              <span v-if="item.start >= 4" class="start1"></span>
-              <span v-else class="start2"></span>
-              <span v-if="item.start >= 5" class="start1"></span>
-              <span v-else class="start2"></span>
-              <!-- <span class="start1"></span>
+    <!-- 电影列表 -->
+    <div class="list">
+      <div class="title">
+        <div class="name">影院热映</div>
+        <div class="more">
+          <a href="/pages/more/main?type=in_theaters">更多</a>
+        </div>
+      </div>
+      <div class="mybox">
+        <scroll-view scroll-x class="myscroll">
+          <div class="item" v-for="(item, index) in movieList" :key="index">
+            <img :src="item.images.small" alt />
+            <div class="moviename">{{ item.title }}</div>
+            <div class="score">
+              <div v-if="item.start != 0">
+                <span v-if="item.start >= 1" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 2" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 3" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 4" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 5" class="start1"></span>
+                <span v-else class="start2"></span>
+                <!-- <span class="start1"></span>
                                             <span class="start1"></span>
                                             <span class="start2"></span>
-              <span class="start2"></span>-->
-              <span class="word">{{ item.rating.average }}</span>
+                <span class="start2"></span>-->
+                <span class="word">{{ item.rating.average }}</span>
+              </div>
+              <div v-else>暂无评分</div>
             </div>
-            <div v-else>暂无评分</div>
           </div>
+        </scroll-view>
+      </div>
+    </div>
+    <!-- top250 -->
+    <div class="list">
+      <div class="title">
+        <div class="name">影院热映</div>
+        <div class="more">
+          <a href="/pages/more/main?type=top250">更多</a>
         </div>
-      </scroll-view>
+      </div>
+      <div class="mybox">
+        <scroll-view scroll-x class="myscroll">
+          <div class="item" v-for="(item, index) in top250List" :key="index">
+            <img :src="item.images.small" alt />
+            <div class="moviename">{{ item.title }}</div>
+            <div class="score">
+              <div v-if="item.start != 0">
+                <span v-if="item.start >= 1" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 2" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 3" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 4" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 5" class="start1"></span>
+                <span v-else class="start2"></span>
+                <!-- <span class="start1"></span>
+                                            <span class="start1"></span>
+                                            <span class="start2"></span>
+                <span class="start2"></span>-->
+                <span class="word">{{ item.rating.average }}</span>
+              </div>
+              <div v-else>暂无评分</div>
+            </div>
+          </div>
+        </scroll-view>
+      </div>
+    </div>
+    <!-- 即将上映 -->
+    <div class="list">
+      <div class="title">
+        <div class="name">影院热映</div>
+        <div class="more">
+          <a href="/pages/more/main?type=coming_soon">更多</a>
+        </div>
+      </div>
+      <div class="mybox">
+        <scroll-view scroll-x class="myscroll">
+          <div class="item" v-for="(item, index) in commonSoon" :key="index">
+            <img :src="item.images.small" alt />
+            <div class="moviename">{{ item.title }}</div>
+            <div class="score">
+              <div v-if="item.start != 0">
+                <span v-if="item.start >= 1" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 2" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 3" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 4" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span v-if="item.start >= 5" class="start1"></span>
+                <span v-else class="start2"></span>
+                <span class="word">{{ item.rating.average }}</span>
+              </div>
+              <div v-else>暂无评分</div>
+            </div>
+          </div>
+        </scroll-view>
+      </div>
     </div>
   </div>
 </template>
@@ -36,33 +115,88 @@ import wxrequest from "../../utils/js/wxrequest.js";
 export default {
   data() {
     return {
-      movieList: []
+      movieList: [],
+      top250List: [],
+      // 即将上映
+      commonSoon: []
     };
   },
-  async created() {
-    var res = await wxrequest({
-      url:
-        "https://autumnfish.cn/v2/movie/in_theaters?q=undefined&apikey=0df993c66c0c636e29ecbb5344252a4a",
-      method: "get",
-      header: {
-        "content-type": "json"
-      }
-    });
-
-    let { statusCode, data } = res;
-    // 判断
-    if (statusCode === 200) {
-      // 得到数据之前将星星数据计算出来
-      data.subjects.forEach(item => {
-        // 得到星星数
-        var startNum = Math.round(item.rating.average / 2);
-        item.start = startNum;
-        item.rating.average = item.rating.average.toFixed(1);
+  methods: {
+    async getMovieList() {
+      var res = await wxrequest({
+        url:
+          "https://autumnfish.cn/v2/movie/in_theaters?q=undefined&apikey=0df993c66c0c636e29ecbb5344252a4a",
+        method: "get",
+        header: {
+          "content-type": "json"
+        }
       });
-      this.movieList = data.subjects;
-    }
-    console.log(this.movieList);
 
+      let { statusCode, data } = res;
+      // 判断
+      if (statusCode === 200) {
+        // 得到数据之前将星星数据计算出来
+        data.subjects.forEach(item => {
+          // 得到星星数
+          var startNum = Math.round(item.rating.average / 2);
+          item.start = startNum;
+          item.rating.average = item.rating.average.toFixed(1);
+        });
+        this.movieList = data.subjects;
+      }
+    },
+    async getTop250List() {
+      var res = await wxrequest({
+        url:
+          "https://autumnfish.cn/v2/movie/top250?q=undefined&apikey=0df993c66c0c636e29ecbb5344252a4a",
+        method: "get",
+        header: {
+          "content-type": "json"
+        }
+      });
+
+      let { statusCode, data } = res;
+      // 判断
+      if (statusCode === 200) {
+        // 得到数据之前将星星数据计算出来
+        data.subjects.forEach(item => {
+          // 得到星星数
+          var startNum = Math.round(item.rating.average / 2);
+          item.start = startNum;
+          item.rating.average = item.rating.average.toFixed(1);
+        });
+        this.top250List = data.subjects;
+      }
+      console.log(this.top250List);
+    },
+    async getCommonSoon() {
+      var res = await wxrequest({
+        url:
+          "https://autumnfish.cn/v2/movie/coming_soon?q=undefined&apikey=0df993c66c0c636e29ecbb5344252a4a",
+        method: "get",
+        header: {
+          "content-type": "json"
+        }
+      });
+
+      let { statusCode, data } = res;
+      // 判断
+      if (statusCode === 200) {
+        // 得到数据之前将星星数据计算出来
+        data.subjects.forEach(item => {
+          // 得到星星数
+          var startNum = Math.round(item.rating.average / 2);
+          item.start = startNum;
+          item.rating.average = item.rating.average.toFixed(1);
+        });
+        this.commonSoon = data.subjects;
+      }
+    }
+  },
+  created() {
+    this.getMovieList();
+    this.getTop250List();
+    this.getCommonSoon();
     // 2.0--------------------------------
     // var wxpromise = wxrequest({
     //   url:
@@ -105,50 +239,65 @@ export default {
 
 <style scoped lang='less'>
 .content {
-  .mybox {
-    overflow: hidden;
-    white-space: nowrap;
-    .myscroll {
-      width: auto;
+  .list {
+    .title {
+      display: flex;
+      justify-content: space-between;
+      padding: 0px 36rpx;
+      height: 98rpx;
+      line-height: 98rpx;
+      .name {
+        font-size: 20px;
+      }
+      .more {
+        color: #42bd56;
+      }
+    }
+    .mybox {
       overflow: hidden;
-      .item {
-        display: inline-block;
-        text-align: center;
-        margin-right: 10px;
-        img {
-          width: 200rpx;
-          height: 285rpx;
-        }
-        .moviename {
-          font-size: 16px;
-          width: 200rpx;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-        }
-        .score {
-          span {
-            display: inline-block;
-            width: 14px;
-            height: 14px;
-            background-size: 100%;
+      white-space: nowrap;
+      .myscroll {
+        width: auto;
+        overflow: hidden;
+        .item {
+          display: inline-block;
+          text-align: center;
+          margin-right: 10px;
+          img {
+            width: 200rpx;
+            height: 285rpx;
           }
-          .start1 {
-            background-image: url(../../utils/imgs/start1.png);
+          .moviename {
+            font-size: 16px;
+            width: 200rpx;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
           }
-          .start2 {
-            background-image: url(../../utils/imgs/start2.png);
+          .score {
+            span {
+              display: inline-block;
+              width: 14px;
+              height: 14px;
+              background-size: 100%;
+            }
+            .start1 {
+              background-image: url(../../utils/imgs/start1.png);
+            }
+            .start2 {
+              background-image: url(../../utils/imgs/start2.png);
+            }
+            font-size: 16px;
+            .word {
+              width: 28px;
+            }
           }
-          font-size: 16px;
-          .word {
-            width: 28px;
+          &:first-child {
+            margin-left: 36rpx;
           }
-        }
-        &:first-child {
-          margin-left: 36rpx;
-        }
-        &:last-child {
-          margin-right: 36rpx;
+          &:last-child {
+            margin-right: 36rpx;
+          }
         }
       }
     }
